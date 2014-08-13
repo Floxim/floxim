43f512296b427526baf8a95324abf682367aa744
Mzhelskiy Maxim <rus.engine@gmail.com>
2014-08-13 22:13:37 +0700

    run migrations from console

--- system/migration_manager.php
+++ system/migration_manager.php
@@ -48,4 +48,39 @@ class fx_migration_manager {
         }
     }
 
+    public function up() {
+        $dir=fx::path('floxim', '/update/migration');
+        // get migrations
+        $migration_files = glob($dir.'/*.php');
+        if (!$migration_files) {
+            return;
+        }
+        $migration_names=array();
+        foreach ($migration_files as $migration_file) {
+            $info=pathinfo($migration_file);
+            $paths=explode('_',$info['filename']);
+            $paths=array_slice($paths,0,2);
+            $migration_names[join('_',$paths)]=$info['filename'];
+        }
+
+        // get completed migration
+        $migrations_completed=fx::data('patch_migration')->all();
+        if ($migrations_completed_name=$migrations_completed->get_values('name')) {
+            $migration_names=array_diff_key($migration_names,array_combine($migrations_completed_name,array_fill(0,count($migrations_completed_name),1)));
+        }
+
+        // run migrations
+        $count=0;
+        foreach($migration_names as $name_full) {
+            $file=$dir.DIRECTORY_SEPARATOR.$name_full.'.php';
+            if (file_exists($file)) {
+                require_once($file);
+                $migration=new $name_full;
+                $migration->exec_up();
+                $count++;
+            }
+        }
+        // todo: need fix
+        echo('Count run new migrations: '.$count);
+    }
 }
