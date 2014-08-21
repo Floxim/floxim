6fce7d3c59ce44eeede52aa8689ba7b8e6897d65
Ilya Myasin <dubr.cola@gmail.com>
2014-08-21 19:08:18 +0400

    1. escape curly braces this way: if (window.test) \{alert('ok');\}
    2. output curly braces "as is" inside {raw}...{/raw} blocks
    
    3. wrap contents of tag which has fx:raw="true" att with {raw}...{/raw} block
    
    4. add fx:raw="true" by default to <script> and <style> if no fx:raw att specified on the tag
    
    So we can:
    <script src="{$template_dir}script.js">
       if (window.ok) {alert('ololo');}
    </script>
    
    And:
    <script fx:raw="false">
    if (window) \{
       window.template_dir = '{$template_dir}';
    \}
    </script>

--- template/template_tokenizer.php
+++ template/template_tokenizer.php
@@ -1,12 +1,14 @@
 <?php
 class fx_template_tokenizer extends fx_template_fsm {
-    public $split_regexp = '~(\{[\$\%\/a-z0-9]+[^\{]*?\}|</?(?:script|style)[^>]*?>|<\?(?:php)?|\?>|<\!--|-->)~';
+    /*public $split_regexp = '~(\{[\$\%\/a-z0-9]+[^\{]*?\}|</?(?:script|style)[^>]*?>|<\?(?:php)?|\?>|<\!--|-->)~';*/
+    public $split_regexp = '~(\\\\{|\\\\}|\{[\$\%\/a-z0-9]+[^\{]*?\}|<\?(?:php)?|\?>|<\!--|-->)~';
     
     const JS = 1;
     const CSS = 2;
     const PHP = 3;
     const HTML = 4;
     const COMMENT = 5;
+    const RAW = 6;
     
     protected $stack = '';
     
@@ -14,14 +16,16 @@ class fx_template_tokenizer extends fx_template_fsm {
         $this->init_state = self::HTML;
         $this->res = array();
         
+        $this->add_rule(self::HTML, '~^\{raw}$~s', self::RAW);
+        $this->add_rule(self::RAW, '~^\{/raw}$~s', self::HTML);
         
         $this->add_rule(self::HTML, '~^\{.+\}$~s', self::HTML, 'add_token');
         
-        $this->add_rule(self::HTML, '~^<script~', self::JS);
-        $this->add_rule(self::JS, '~^</script~', self::HTML);
+        //$this->add_rule(self::HTML, '~^<script~', self::JS);
+        //$this->add_rule(self::JS, '~^</script~', self::HTML);
         
-        $this->add_rule(self::HTML, '~^<style~', self::CSS);
-        $this->add_rule(self::CSS, '~^</style~', self::HTML);
+        //$this->add_rule(self::HTML, '~^<style~', self::CSS);
+        //$this->add_rule(self::CSS, '~^</style~', self::HTML);
         
         $this->add_rule(self::HTML, '~^<\?~', self::PHP);
         $this->add_rule(self::PHP, '~^\?>~', self::HTML);
@@ -39,6 +43,12 @@ class fx_template_tokenizer extends fx_template_fsm {
     }
     
     public function default_callback($ch) {
+        if (preg_match("~^\{/?raw\}$~", $ch)) {
+            return;
+        }
+        if ($this->state !== self::RAW && ($ch === '\{' || $ch === '\}')) {
+            $ch = str_replace("\\", '', $ch);
+        }
         $this->stack .= $ch;
     }
 }
