67b95da99552957fd13c443dd18855c9f7e4d0b2
dubr <dubr.cola@gmail.com>
2017-02-21 14:49:41 +0300

    check entity

--- Template/Template.php
+++ Template/Template.php
@@ -660,7 +660,7 @@ class Template
             $entity = $this->getVisual();
             $prop = $this->isWrapper() ? 'wrapper_visual' : 'template_visual';
         }
-        if ($entity) {
+        if ($entity && $entity instanseof \Floxim\Floxim\System\Entity) {
             $entity->digSet($prop.'.'.$this->context->getScopePrefix('.').$name, $value);
         }
     }
